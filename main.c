973b4a01faa3b829e8442f1909b7c11da7649655
Aaron An <thinkiny@126.com>
2015-03-30 13:17:05 +0800

    fix segment fault

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 static void bqPlayerCallback(SLAndroidSimpleBufferQueueItf bq, void *context)
 {
-	fprintf(stderr, "9992");
+	fprintf(stderr, "9992\n");
 }
 
 int main(int argc, char* const argv[])
@@ -24,14 +24,17 @@ int main(int argc, char* const argv[])
 	result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, (void*)&engineEngine);
 
 	SLDataFormat_PCM format_pcm = { SL_DATAFORMAT_PCM, 2, SL_SAMPLINGRATE_44_1,
-		SL_PCMSAMPLEFORMAT_FIXED_16, SL_PCMSAMPLEFORMAT_FIXED_16,
-		SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT, SL_BYTEORDER_LITTLEENDIAN };
+                                    SL_PCMSAMPLEFORMAT_FIXED_16, SL_PCMSAMPLEFORMAT_FIXED_16,
+                                    SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT, SL_BYTEORDER_LITTLEENDIAN };
 	SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
 	SLDataSource audioSrc = {&loc_bufq, &format_pcm};
 
 	SLObjectItf outputMixObject = NULL;
+
 	result = (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 0, NULL, NULL);
+	fprintf(stderr, "result %d\n", result);
 	result = (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
+	fprintf(stderr, "result %d\n", result);
 
 	SLDataLocator_OutputMix loc_outmix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
 	SLDataSink audioSnk = {&loc_outmix, NULL};
@@ -39,23 +42,23 @@ int main(int argc, char* const argv[])
 	SLObjectItf bqPlayerObject = NULL;
 	SLPlayItf bqPlayerPlay;
 	SLAndroidSimpleBufferQueueItf bqPlayerBufferQueue;
-	const SLInterfaceID ids1[] = {SL_IID_ANDROIDBUFFERQUEUESOURCE};
+	const SLInterfaceID ids1[] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
 	const SLboolean req1[] = {SL_BOOLEAN_TRUE};
-	fprintf(stderr, "1\n");
+	fprintf(stderr, "result %d\n", result);
 	result = (*engineEngine)->CreateAudioPlayer(engineEngine, &bqPlayerObject, &audioSrc, &audioSnk, 1, ids1, req1);
-	fprintf(stderr, "2\n");
+	fprintf(stderr, "result %d\n", result);
 	result = (*bqPlayerObject)->Realize(bqPlayerObject, SL_BOOLEAN_FALSE);
-	fprintf(stderr, "3\n");
+    fprintf(stderr, "result %d\n", result);    
 	result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_PLAY, &bqPlayerPlay);
-	fprintf(stderr, "4\n");
-	result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_BUFFERQUEUE, &bqPlayerBufferQueue);
-	fprintf(stderr, "5\n");
+    fprintf(stderr, "result %d\n", result);
+	result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_ANDROIDSIMPLEBUFFERQUEUE, &bqPlayerBufferQueue);
+    fprintf(stderr, "result %d\n", result);
 	result = (*bqPlayerBufferQueue)->RegisterCallback(bqPlayerBufferQueue, bqPlayerCallback, NULL);
 	//result = (*bqPlayerObject)->RegisterCallback(bqPlayerObject, bqPlayerCallback, NULL);
-	fprintf(stderr, "6\n");
+    fprintf(stderr, "result %d\n", result);
 	result = (*bqPlayerPlay)->SetPlayState(bqPlayerPlay, SL_PLAYSTATE_PLAYING);
 	//bqPlayerBufferQueue->Enqueue(bqPlayerBufferQueue, NULL, );
-
+    fprintf(stderr, "result %d\n", result);
 	usleep(60000000);
 	return 0;
 }
